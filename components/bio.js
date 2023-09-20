30697427bf1532ae1e1ae60dc9c23f3b45ef4da3
wattsmainsanglais <wattsmainsanglais@gmail.com>
2023-09-20 20:33:29 +0200

    added skills logos to bio section

--- components/bio.js
+++ components/bio.js
@@ -1,4 +1,5 @@
 
+
 import styles from './bio.module.css'
 
 export default function Bio(){
@@ -16,11 +17,11 @@ export default function Bio(){
                 <article className={styles.skills}>
                     <h2 className={styles.animatedHeader}>Skills</h2>
                     <aside className={styles.skillsLogo}>
-                        <img  src="images/social_html5_html_71.png" aria-label="html logo" />
-                        <img  src="images/css_3721.png" aria-label="css logo" />
-                        <img src="images/javascript_logo_icon_145155.png" aria-label="Javascript logo" />
-                        <img  src="images/expressjs_logo_icon_169185.png" aria-label="express js logo" />
-                        <img  src="images/node_icon_196185.png" aria-label="node js logo" />
+                        <img className={styles.skillsImg} src="images/social_html5_html_71.png" aria-label="html logo" />
+                        <img  className={styles.skillsImg} src="images/css_3721.png" aria-label="css logo" />
+                        <img className={styles.skillsImg} src="images/javascript_logo_icon_145155.png" aria-label="Javascript logo" />
+                        <img  className={styles.skillsImg} src="images/expressjs_logo_icon_169185.png" aria-label="express js logo" />
+                        <img  className={styles.skillsImg} src="images/node_icon_196185.png" aria-label="node js logo" />
                     </aside>
                 </article>
                     <hr />
